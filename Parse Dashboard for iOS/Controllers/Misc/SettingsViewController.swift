11b5a480995d9b32f82945e50d18949ee1d22a5b
Nathan Tannar <nathantannar4@gmail.com>
2017-12-17 20:41:24 -0800

    Settings Page Updates

--- Parse Dashboard for iOS/Controllers/Misc/SettingsViewController.swift
+++ Parse Dashboard for iOS/Controllers/Misc/SettingsViewController.swift
@@ -28,6 +28,8 @@
 import UIKit
 import Former
 import AlertHUDKit
+import AcknowList
+import MessageUI
 
 class SettingsViewController: FormViewController {
     
@@ -79,7 +81,43 @@ class SettingsViewController: FormViewController {
                 }
         }
         
-        let resetRow = LabelRowFormer<FormLabelCell>() {
+        let bugReportRow = LabelRowFormer<FormLabelCell>() {
+                $0.titleLabel.font = .boldSystemFont(ofSize: 15)
+                $0.accessoryType = .disclosureIndicator
+            }.configure {
+                $0.text = "Report a Bug"
+            }.onSelected { _ in
+                self.former.deselect(animated: true)
+                if MFMailComposeViewController.canSendMail() {
+                    let vc = MFMailComposeViewController()
+                    vc.mailComposeDelegate = self
+                    vc.setToRecipients(["nathantannar4@gmail.com"])
+                    vc.setSubject("[Parse Dashboard for iOS] - Bug Report")
+                    self.present(vc, animated: true, completion: nil)
+                } else {
+                    Ping(text: "No Mail Accounts Setup", style: .danger).show()
+                }
+        }
+        
+        let featureRequestRow = LabelRowFormer<FormLabelCell>() {
+                $0.titleLabel.font = .boldSystemFont(ofSize: 15)
+                $0.accessoryType = .disclosureIndicator
+            }.configure {
+                $0.text = "Request a Feature"
+            }.onSelected { _ in
+                self.former.deselect(animated: true)
+                if MFMailComposeViewController.canSendMail() {
+                    let vc = MFMailComposeViewController()
+                    vc.mailComposeDelegate = self
+                    vc.setToRecipients(["nathantannar4@gmail.com"])
+                    vc.setSubject("[Parse Dashboard for iOS] - Feature Request")
+                    self.present(vc, animated: true, completion: nil)
+                } else {
+                    Ping(text: "No Mail Accounts Setup", style: .danger).show()
+                }
+        }
+        
+        let resetTutorialRow = LabelRowFormer<FormLabelCell>() {
                 $0.titleLabel.font = .boldSystemFont(ofSize: 15)
             }.configure {
                 $0.text = "Reset Tutorial"
@@ -93,6 +131,19 @@ class SettingsViewController: FormViewController {
                 self.former.deselect(animated: true)
         }
         
+        let acknowledgementRow = LabelRowFormer<FormLabelCell>() {
+                $0.titleLabel.font = .boldSystemFont(ofSize: 15)
+                $0.accessoryType = .disclosureIndicator
+            }.configure {
+                $0.text = "Acknowledgements"
+            }.onSelected { _ in
+                self.former.deselect(animated: true)
+                let path = Bundle.main.path(forResource: "Pods-Parse Dashboard for iOS-acknowledgements", ofType: "plist")!
+                let vc = AcknowListViewController(acknowledgementsPlistPath: path)
+                vc.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
+                self.navigationController?.pushViewController(vc, animated: true)
+        }
+        
         let createHeader: ((String) -> ViewFormer) = { text in
             return LabelViewFormer<FormLabelHeaderView>()
                 .configure {
@@ -102,11 +153,23 @@ class SettingsViewController: FormViewController {
             }
         }
         
-        let securitySection = SectionFormer(rowFormer: switchRow).set(headerViewFormer: createHeader("Security"))
+        let securitySection = SectionFormer(rowFormer: switchRow)
+            .set(headerViewFormer: createHeader("Security"))
+        
+        let supportSection = SectionFormer(rowFormer: bugReportRow, featureRequestRow)
+            .set(headerViewFormer: createHeader("Support"))
     
-        let otherSection = SectionFormer(rowFormer: resetRow).set(headerViewFormer: createHeader("Other"))
+        let otherSection = SectionFormer(rowFormer: resetTutorialRow, acknowledgementRow)
+            .set(headerViewFormer: createHeader("Other"))
+            .set(footerViewFormer: {
+                return LabelViewFormer<FormLabelFooterView>()
+                    .configure {
+                        $0.viewHeight = 32
+                        $0.text = "Version " + (Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "")
+                }
+            }())
             
-        former.append(sectionFormer: securitySection, otherSection)
+        former.append(sectionFormer: securitySection, supportSection, otherSection)
     }
     
     // MARK: - User Actions
@@ -114,5 +177,17 @@ class SettingsViewController: FormViewController {
     @objc
     func dismissInfo() {
         dismiss(animated: true, completion: nil)
+    }
+}
+
+extension SettingsViewController: MFMailComposeViewControllerDelegate {
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        
+        if let error = error {
+            Ping(text: error.localizedDescription, style: .danger).show()
+        } else {
+            controller.dismiss(animated: true, completion: nil)
+        }
     }
 }
